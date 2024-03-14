53e68aac48eaf7eb352fa8d05a570ec79097af30
Mgokayay <aymustafagokay@gmail.com>
2024-03-14 14:13:58 +0300

    entity is created and completed

---
+++ src/main/java/com/example/studentsystem/model/Student.java
@@ -0,0 +1,25 @@
+package com.example.studentsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "student", schema = "stapp")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+}
